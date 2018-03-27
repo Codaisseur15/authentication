ba35ec7d2616a05a67eebdabbfd99dfcac60ef7a
Ju111 <SchneiderJu@gmx.net>
2018-03-27 16:32:53 +0200

    saved unsaved files

--- src/users/controller.ts
+++ src/users/controller.ts
@@ -1,55 +1,32 @@
-import { JsonController, Post, Param, Get, Put, Patch, NotFoundError, Body, Delete, Authorized } from 'routing-controllers'
+import { JsonController, Post, Param, Get, Body, Authorized, HttpCode } from 'routing-controllers'
 import User from './entity';
 
 @JsonController()
 export default class UserController {
 
   @Post('/users')
-  async signup(
-    @Body() user: User
-  ) {
-    const {password, ...rest} = user
-    const entity = User.create(rest)
-    await entity.setPassword(password)
-    return entity.save()
-  }
+    @HttpCode(201)
+    async signup(
+      @Body() user: User
+    ) {
+      const {password, ...rest} = user
+      const entity = User.create(rest)
+      await entity.setPassword(password)
+      return entity.save()
+    }
 
-  @Authorized()
+  // @Authorized()
   @Get('/users/:id([0-9]+)')
   getUser(
-    @Param('id') id: number
+    @Param('id') id: number,
+    // @Param('role') role: string
   ) {
     return User.findOneById(id)
   }
 
-  @Authorized()
+  // @Authorized()
   @Get('/users')
   allUsers() {
     return User.find()
   }
-
-  @Authorized()
-  @Put('/users/:id([0-9]+)')
-  @Patch('/users/:id([0-9]+)')
-  async updateUser(
-    @Param('id') id: number,
-    @Body() update: Partial<User>
-  ) {
-    const user = await User.findOneById(id)
-    if (!user) throw new NotFoundError('Cannot find user')
-
-    return User.merge(user, update).save()
-  }
-
-  @Authorized()
-  @Delete('/users/:id([0-9]+)')
-  async removeUser(
-    @Param('id') id: number
-  )  {
-      const user = await User.findOneById(id)
-      if (!user) throw new NotFoundError('Cannot find user')
-      user.remove()
-      return "user succesfully deleted"
-    }
-
 }
--- src/users/entity.ts
+++ src/users/entity.ts
@@ -1,10 +1,7 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 import { Exclude } from 'class-transformer';
-//'exclude' is an option that excludes the property from the transformed result, here from the classToPlain operation.
 import { MinLength, IsString, IsEmail } from 'class-validator';
 import * as bcrypt from 'bcrypt'
-
-export type Role = 'teacher' | 'admin'
 
 @Entity()
 export default class User extends BaseEntity {
@@ -12,7 +9,6 @@ export default class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id?: number
 
-  @IsString()
   @IsEmail()
   @Column('text')
   email: string
@@ -23,8 +19,9 @@ export default class User extends BaseEntity {
   @Exclude({ toPlainOnly: true })
   password: string
 
+  // @IsString()
   @Column('text', {default: 'teacher'})
-  role: Role
+  role: string
 
   async setPassword(rawPassword: string) {
     const hash = await bcrypt.hash(rawPassword, 10)
@@ -34,5 +31,4 @@ export default class User extends BaseEntity {
   checkPassword(rawPassword: string): Promise<boolean> {
     return bcrypt.compare(rawPassword, this.password)
   }
-
 }
