922b5c266851336ebecd4a2754f5ac6130050a8c
Julia Schneider <SchneiderJu@gmx.net>
2018-03-27 16:30:14 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -71,31 +71,3 @@ setupDb()
     app.listen(port, () => console.log(`Listening on port ${port}`));
   })
   .catch(err => console.error(err));
-  // authorizationChecker: (action: Action) => {
-  //   const header: string = action.request.headers.authorization
-  //   if (header && header.startsWith('Bearer ')) {
-  //     const [ , token ] = header.split(' ')
-  //
-  //     try {
-  //       return !!(token && verify(token))
-  //     }
-  //     catch (e) {
-  //       throw new BadRequestError(e)
-  //     }
-  //   }
-  //
-
-//   currentUserChecker: async (action: Action) => {
-//     const header: string = action.request.headers.authorization;
-//     if (header && header.startsWith("Bearer ")) {
-//       const [, token] = header.split(" ");
-//
-//       if (token) {
-//         const { id } = verify(token);
-//
-//         return User.findOneById(id);
-//       }
-//     }
-//     return undefined;
-//   }
-// })
