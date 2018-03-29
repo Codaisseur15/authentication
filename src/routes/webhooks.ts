3d75396b18cbacf37cff9e3b909a3c0ab4293f94
Ju111 <SchneiderJu@gmx.net>
2018-03-29 10:24:01 +0200

    add body to put and patch plus correct error message

--- src/routes/webhooks.ts
+++ src/routes/webhooks.ts
@@ -23,64 +23,91 @@ export default class WebhookController {
 
   @Post('/events')
   async postEvents(
-    @CurrentUser() user: { id, role }) {
+    @CurrentUser() user: { id, role },
+    @Body() body: object
+  ) {
       request
         .post(`${webhooksUrl}/events`)
-        .set({'x-user-id': user.id, 'x-user-role': user.role})
-        .catch(err => alert(err))
+        .set({'x-user-id': user.id || null, 'x-user-role': user.role || null})
+        .send(body)
+        .then(res => res.body)
+        .catch(err => {
+          return { message: err.message }
+        })
     }
 
   @Get('/targets')
   async getTargets(
-    @CurrentUser() user: { id, role }) {
+    @CurrentUser() user: { id, role }
+  ) {
       request
         .get(`${webhooksUrl}/targets`)
-        .set({'x-user-id': user.id, 'x-user-role': user.role})
-        .catch(err => alert(err))
+        .set({'x-user-id': user.id || null, 'x-user-role': user.role || null})
+        .catch(err => {
+          return { message: err.message }
+        })
     }
 
   @Get('/targets/:id')
   async getTarget(
-    @CurrentUser() user: { id, role }) {
+    @CurrentUser() user: { id, role }
+  ) {
       request
         .get(`${webhooksUrl}/targets/:id`)
-        .set({'x-user-id': user.id, 'x-user-role': user.role})
-        .catch(err => alert(err))
+        .set({'x-user-id': user.id || null, 'x-user-role': user.role || null})
+        .catch(err => {
+          return { message: err.message }
+        })
     }
 
   @Get('/events')
   async getEvent(
-    @CurrentUser() user: { id, role }) {
+    @CurrentUser() user: { id, role }
+  ) {
       request
         .get(`${webhooksUrl}/events`)
-        .set({'x-user-id': user.id, 'x-user-role': user.role})
-        .catch(err => alert(err))
+        .set({'x-user-id': user.id || null, 'x-user-role': user.role || null})
+        .catch(err => {
+          return { message: err.message }
+        })
     }
 
   @Get('/events/sent')
   async getSentEvents(
-    @CurrentUser() user: { id, role }) {
+    @CurrentUser() user: { id, role }
+  ) {
       request
         .get(`${webhooksUrl}/events/sent`)
-        .set({'x-user-id': user.id, 'x-user-role': user.role})
-        .catch(err => alert(err))
+        .set({'x-user-id': user.id || null, 'x-user-role': user.role || null})
+        .catch(err => {
+          return { message: err.message }
+        })
     }
 
   @Get('/events/failed')
   async getFailedEvents(
-    @CurrentUser() user: { id, role }) {
+    @CurrentUser() user: { id, role }
+  ) {
       request
         .get(`${webhooksUrl}/events/failed`)
-        .set({'x-user-id': user.id, 'x-user-role': user.role})
-        .catch(err => alert(err))
+        .set({'x-user-id': user.id || null, 'x-user-role': user.role || null})
+        .catch(err => {
+          return { message: err.message }
+        })
     }
 
   @Patch('/targets/:id')
   async patchTarget(
-    @CurrentUser() user: { id, role }) {
+    @CurrentUser() user: { id, role },
+    @Body() body: object
+  ) {
       request
         .patch(`${webhooksUrl}/targets/:id`)
-        .set({'x-user-id': user.id, 'x-user-role': user.role})
-        .catch(err => alert(err))
+        .set({'x-user-id': user.id || null, 'x-user-role': user.role || null})
+        .send(body)
+        .then(res => res.body)
+        .catch(err => {
+          return { message: err.message }
+        })
     }
 }
